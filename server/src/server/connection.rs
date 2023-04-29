101d428a1a2a9fe19903ec6a9e89ec83dce6f536
Alexander Soen <alexandersoen@gmail.com>
2023-04-29 17:12:32 +1000

    Change echo to async func

--- server/src/server/connection.rs
+++ server/src/server/connection.rs
@@ -1,17 +1,21 @@
-use std::sync::Arc;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
-use tokio::net::TcpStream;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
+use tokio::net::tcp::{ReadHalf, WriteHalf};
 use tokio::sync::Mutex;
+use tokio::sync::mpsc::{self, Sender};
 
 pub type Id = u64;
+
+const CHANNEL_BUFFER_SIZE: usize = 100;
 
 #[allow(dead_code)]
 pub struct Connection {
     pub id: Id,
-    stream: Arc<Mutex<TcpStream>>,
     address: SocketAddr,
+    stream: Arc<Mutex<TcpStream>>,
     running: bool,
 }
 
@@ -19,8 +23,8 @@ impl Connection {
     pub fn new(id: Id, stream: TcpStream, address: SocketAddr) -> Self {
         Connection {
             id,
-            stream: Arc::new(Mutex::new(stream)),
             address,
+            stream: Arc::new(Mutex::new(stream)),
             running: false,
         }
     }
@@ -32,24 +36,27 @@ impl Connection {
         self.running = true;
 
         let task_stream_mtx = self.stream.clone();
-
         tokio::spawn(async move {
             let mut task_stream = task_stream_mtx.lock().await;
             let (reader, mut writer) = task_stream.split();
             let mut buffer_reader = BufReader::new(reader);
             loop {
                 let mut buf = vec![];
-                match buffer_reader.read_until(b'\n', &mut buf).await {
-                    Ok(0) => {},
-                    Ok(_) => {
-                        let s = String::from_utf8_lossy(&buf);
-                        writer.write_all(s.as_bytes()).await.unwrap();
-                    },
-                    Err(e) => panic!("{:?}", e),
-                }
+                echo(&mut buffer_reader, &mut writer, &mut buf).await;
             }
         });
 
         Ok(())
+    }
+}
+
+async fn echo<'a>(buffer_reader: &mut BufReader<ReadHalf<'_>>, writer: &mut WriteHalf<'_>, buffer: &mut Vec<u8>) {
+    match buffer_reader.read_until(b'\n', buffer).await {
+        Ok(0) => {},
+        Ok(_) => {
+            let s = String::from_utf8_lossy(&buffer);
+            writer.write_all(s.as_bytes()).await.unwrap();
+        },
+        Err(e) => panic!("{:?}", e),
     }
 }
